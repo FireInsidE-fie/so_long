06fa0a927ca9f2af7718d31f023385c52816bc49
Aisling <aisling.fontaine@protonmail.com>
2024-11-28 13:10:24 +0100

    add some comments to header and x11 includes

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:19:22 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/28 12:42:48 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/28 13:09:20 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,23 @@
 # include <stdlib.h>
 # include <inttypes.h>
 
+// X11
+#include <X11/X.h>
+#include <X11/keysym.h>
+
 // Structs
-typedef struct s_core
+typedef struct s_core		// For core MLX and so_long functionalities
 {
 	void	*mlx_ptr;		// MLX pointer
-	void	*win_ptr;		// MLX window pointer
+	void	*win_ptr;		// MLX main window pointer
 	void	*textures[10];	// MLX image pointers
-	char	**map;			// Map pointer
+	char	**map;			// So_long map pointer
 }	t_core;
 
-typedef struct s_img
+typedef struct s_img		// For an image and its associated data
 {
-	void	*img;
-	char	*addr;
+	void	*img;			// The image pointer itself
+	char	*addr;			// The address of the image
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
