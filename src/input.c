4f0f7c26ce6498cbbb45522e8615ed69ea4d7de5
Aisling <aisling.fontaine@protonmail.com>
2024-11-30 15:22:17 +0100

    add check to stop going into walls

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:16:14 by estettle          #+#    #+#             */
-/*   Updated: 2024/11/30 14:50:04 by estettle         ###   ########.fr       */
+/*   Updated: 2024/11/30 15:21:37 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	key_pressed(int key, t_core *core)
 {
-	if (key == 'w')
+	if (key == 'w' && core->map.map[core->player.y - 1][core->player.x] != '1')
 		put_img_to_index(core, MADDIE1, core->player.x, core->player.y - 1); // Move up
-	else if (key == 'a')
-		put_img_to_index(core, MADDIE1, core->player.x - 1, core->player.y); // Move up
-	else if (key == 's')
-		put_img_to_index(core, MADDIE1, core->player.x, core->player.y + 1); // Move up
-	else if (key == 'd')
-		put_img_to_index(core, MADDIE1, core->player.x + 1, core->player.y); // Move up
+	else if (key == 'a' && core->map.map[core->player.y][core->player.x - 1] != '1')
+		put_img_to_index(core, MADDIE1, core->player.x - 1, core->player.y); // Move left
+	else if (key == 's' && core->map.map[core->player.y + 1][core->player.x] != '1')
+		put_img_to_index(core, MADDIE1, core->player.x, core->player.y + 1); // Move down
+	else if (key == 'd'	&& core->map.map[core->player.y][core->player.x + 1] != '1')
+		put_img_to_index(core, MADDIE1, core->player.x + 1, core->player.y); // Move right
 	return (0);
 }
