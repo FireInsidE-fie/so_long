c6c5d8586dadc295deee3f6417414058f5c87e8c
Aisling <aisling.fontaine@protonmail.com>
2024-12-02 17:18:48 +0100

    fuse move left right etc function into a single move_player function

--- src/input.c
+++ src/input.c
@@ -6,49 +6,29 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:16:14 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/02 14:53:05 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/02 17:17:05 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-void	move_up(t_core *core)
+static void	move_player(int key, t_core *core)
 {
 	if (core->map.map[core->player.y][core->player.x] != 'E')
 		put_img_to_index(core, EMPTY, core->player.x, core->player.y);
 	else
 		put_img_to_index(core, CHEST, core->player.x, core->player.y);
-	core->player.y--;
-	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
-}
-
-void	move_left(t_core *core)
-{
-	if (core->map.map[core->player.y][core->player.x] != 'E')
-		put_img_to_index(core, EMPTY, core->player.x, core->player.y);
-	else
-		put_img_to_index(core, CHEST, core->player.x, core->player.y);
-	core->player.x--;
-	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
-}
-
-void	move_down(t_core *core)
-{
-	if (core->map.map[core->player.y][core->player.x] != 'E')
-		put_img_to_index(core, EMPTY, core->player.x, core->player.y);
-	else
-		put_img_to_index(core, CHEST, core->player.x, core->player.y);
-	core->player.y++;
-	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
-}
-
-void	move_right(t_core *core)
-{
-	if (core->map.map[core->player.y][core->player.x] != 'E')
-		put_img_to_index(core, EMPTY, core->player.x, core->player.y);
-	else
-		put_img_to_index(core, CHEST, core->player.x, core->player.y);
-	core->player.x++;
+	if (key == 'w' || key == 'a' || key == 's' || key == 'd')
+	{
+		if (key == 'w' && core->map.map[core->player.y - 1][core->player.x] != '1')
+			core->player.y--;
+		else if (key == 'a' && core->map.map[core->player.y][core->player.x - 1] != '1')
+			core->player.x--;
+		else if (key == 's' && core->map.map[core->player.y + 1][core->player.x] != '1')
+			core->player.y++;
+		else if (key == 'd' && core->map.map[core->player.y][core->player.x + 1] != '1')
+			core->player.x++;
+	}
 	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
 }
 
@@ -57,15 +37,8 @@ int	key_pressed(int key, t_core *core)
 	// Leaving a comment here for future me : why not get rid of the t_map struct?
 	// The only use for the width and height values would be when checking the map and
 	// initializing right?
-	if (key == 'w' && core->map.map[core->player.y - 1][core->player.x] != '1')
-		move_up(core);
-	else if (key == 'a' && core->map.map[core->player.y][core->player.x - 1] != '1')
-		move_left(core);
-	else if (key == 's' && core->map.map[core->player.y + 1][core->player.x] != '1')
-		move_down(core);
-	else if (key == 'd' && core->map.map[core->player.y][core->player.x + 1] != '1')
-		move_right(core);
-	else if (key == 65307) // ESC
+	move_player(key, core);
+	if (key == 65307) // ESC
 		ft_kill(core, 0);
 	if (core->map.map[core->player.y][core->player.x] == 'C')
 	{
