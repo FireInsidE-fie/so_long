5e516335cb246b5c1f8405df468e342ccaacf792
Aisling <aisling.fontaine@protonmail.com>
2024-12-04 00:41:00 +0100

    add update_position function to shorten move_player function

--- src/input.c
+++ src/input.c
@@ -6,11 +6,39 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:16:14 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/03 21:00:58 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/03 21:30:14 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+
+static void	update_position(int key, t_core *core)
+{
+	if (key == 'w'
+		&& core->map.map[core->player.y - 1][core->player.x] != '1')
+	{
+		ft_printf("Number of moves so far : %d\n", ++core->player.moves);
+		core->player.y--;
+	}
+	else if (key == 'a'
+		&& core->map.map[core->player.y][core->player.x - 1] != '1')
+	{
+		ft_printf("Number of moves so far : %d\n", ++core->player.moves);
+		core->player.x--;
+	}
+	else if (key == 's'
+		&& core->map.map[core->player.y + 1][core->player.x] != '1')
+	{
+		ft_printf("Number of moves so far : %d\n", ++core->player.moves);
+		core->player.y++;
+	}
+	else if (key == 'd'
+		&& core->map.map[core->player.y][core->player.x + 1] != '1')
+	{
+		ft_printf("Number of moves so far : %d\n", ++core->player.moves);
+		core->player.x++;
+	}
+}
 
 static void	move_player(int key, t_core *core)
 {
@@ -19,28 +47,7 @@ static void	move_player(int key, t_core *core)
 	else
 		put_img_to_index(core, CHEST, core->player.x, core->player.y);
 	if (key == 'w' || key == 'a' || key == 's' || key == 'd')
-	{
-		if (key == 'w' && core->map.map[core->player.y - 1][core->player.x] != '1')
-		{
-			ft_printf("Number of moves so far : %d\n", ++core->player.moves);
-			core->player.y--;
-		}
-		else if (key == 'a' && core->map.map[core->player.y][core->player.x - 1] != '1')
-		{
-			ft_printf("Number of moves so far : %d\n", ++core->player.moves);
-			core->player.x--;
-		}
-		else if (key == 's' && core->map.map[core->player.y + 1][core->player.x] != '1')
-		{
-			ft_printf("Number of moves so far : %d\n", ++core->player.moves);
-			core->player.y++;
-		}
-		else if (key == 'd' && core->map.map[core->player.y][core->player.x + 1] != '1')
-		{
-			ft_printf("Number of moves so far : %d\n", ++core->player.moves);
-			core->player.x++;
-		}
-	}
+		update_position(key, core);
 	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
 }
 
