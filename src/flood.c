a0844da27a002c47f7a44ad7a52b0aea6058e8ab
Aisling <aisling.fontaine@protonmail.com>
2024-12-09 13:04:23 +0100

    make flood_check to the Norm

--- src/flood.c
+++ src/flood.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:04:30 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/09 12:50:03 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/09 13:04:03 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,23 @@
  * @param map A copy of the current map to flood check through.
  * @param x The starting x position.
  * @param y The starting y position.
- * @param exit_found A pointer to an int that will be set to 1 once an exit is
- * found.
- * @param collectibles A pointer to an int that will be incremented as
- * collectibles are found.
+ * @param sum A pointer to an int that will be incremented by 1 each time a key
+ * or an exit is found. The goal is for this int to be set to the target number
+ * of keys + 1 (the exit) for a map to be valid.
  */
-void	flood_check(char **map, int x, int y, int *exit_found, int *collectibles)
+void	flood_check(char **map, int x, int y, int *sum)
 {
 	if (map[y][x] == 'E' && ft_printf("Exit found!\n"))
-		*exit_found = 1;
+		(*sum)++;
 	else if (map[y][x] == 'C' && ft_printf("Key found!\n"))
-		(*collectibles)++;
+		(*sum)++;
 	else if (map[y][x] == '1')
 		return ;
 	map[y][x] = '1';
-	flood_check(map, x - 1, y, exit_found, collectibles);
-	flood_check(map, x, y - 1, exit_found, collectibles);
-	flood_check(map, x + 1, y, exit_found, collectibles);
-	flood_check(map, x, y + 1 , exit_found, collectibles);
+	flood_check(map, x - 1, y, sum);
+	flood_check(map, x, y - 1, sum);
+	flood_check(map, x + 1, y, sum);
+	flood_check(map, x, y + 1, sum);
 }
 
 /**
@@ -51,26 +50,21 @@ int	check_paths(t_core *core)
 {
 	int	x;
 	int	y;
-	int	exit_found;
-	int	collectibles;
+	int	sum;
 
 	y = -1;
 	while (++y < core->map.height)
 	{
 		x = -1;
 		while (++x < core->map.width)
-		{
 			if (core->map.map[y][x] == 'P')
 				break ;
-		}
 		if (core->map.map[y][x] == 'P')
 			break ;
 	}
-	exit_found = 0;
-	collectibles = 0;
-	flood_check(copy_map(core), x, y, &exit_found, &collectibles);
-	ft_printf("%d\t%d\n", exit_found, collectibles);
-	if (exit_found != 1 || collectibles != core->map.collectibles)
+	sum = 0;
+	flood_check(copy_map(core), x, y, &sum);
+	if (sum != core->map.collectibles + 1)
 		return (-1);
 	return (0);
 }
