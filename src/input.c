2376eeab8a85aa6944c34aaf26708c9019f2e80f
Aisling <aisling.fontaine@protonmail.com>
2024-12-12 22:09:16 +0100

    add arrow keys support

--- src/input.c
+++ src/input.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 13:16:14 by estettle          #+#    #+#             */
-/*   Updated: 2024/12/09 13:33:40 by estettle         ###   ########.fr       */
+/*   Updated: 2024/12/12 22:06:38 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 static void	update_position(int key, t_core *core)
 {
-	if (key == 'w'
+	if ((key == 'w' || key == 65362)
 		&& core->map.map[core->player.y - 1][core->player.x] != '1')
 		core->player.y--;
-	else if (key == 'a'
+	else if ((key == 'a' || key == 65361)
 		&& core->map.map[core->player.y][core->player.x - 1] != '1')
 		core->player.x--;
-	else if (key == 's'
+	else if ((key == 's' || key == 65364)
 		&& core->map.map[core->player.y + 1][core->player.x] != '1')
 		core->player.y++;
-	else if (key == 'd'
+	else if ((key == 'd' || key == 65363)
 		&& core->map.map[core->player.y][core->player.x + 1] != '1')
 		core->player.x++;
 	ft_printf("Number of moves so far : %d\n", ++core->player.moves);
@@ -35,7 +35,8 @@ static void	move_player(int key, t_core *core)
 		put_img_to_index(core, EMPTY, core->player.x, core->player.y);
 	else
 		put_img_to_index(core, CHEST, core->player.x, core->player.y);
-	if (key == 'w' || key == 'a' || key == 's' || key == 'd')
+	if (key == 'w' || key == 'a' || key == 's' || key == 'd'
+		|| key == 65361 || key == 65362 || key == 65363 || key == 65364)
 		update_position(key, core);
 	put_img_to_index(core, MADDIE1, core->player.x, core->player.y);
 }
@@ -60,5 +61,6 @@ int	key_pressed(int key, t_core *core)
 		ft_printf("You got the strawberry!\n");
 		ft_kill(core, 0);
 	}
+	ft_printf("%d\n", key);
 	return (0);
 }
