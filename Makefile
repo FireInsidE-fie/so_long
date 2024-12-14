476784da7b6d7f3d14e6520dc1bb59dde7af3028
Aisling <aisling.fontaine@protonmail.com>
2024-12-14 10:49:04 +0100

    prepare makefile and file structure for bonus

--- Makefile
+++ Makefile
@@ -1,40 +1,47 @@
-NAME		=	so_long
-CC			=	gcc
-CFLAGS		=	-Wall -Wextra -Werror
-RM			=	rm
-RMFLAGS		=	-f
-SRCDIR		=	src/
-CFILES		=	$(SRCDIR)so_long.c \
-				$(SRCDIR)window_utils.c \
-				$(SRCDIR)init.c \
-				$(SRCDIR)input.c \
-				$(SRCDIR)flood.c \
-				$(SRCDIR)map.c
-OBJS		=	$(CFILES:.c=.o)
-INCLDIR		=	include/
-IFILES		=	$(INCLDIR)so_long.h
-LIBFTDIR	=	libftprintf/
-LIBFT		=	$(LIBFTDIR)libftprintf.a
+NAME			=	so_long
+CC				=	gcc
+CFLAGS			=	-Wall -Wextra -Werror
+RM				=	rm
+RMFLAGS			=	-f
+SRCDIR			=	src/
+CFILES			=	$(SRCDIR)so_long.c \
+					$(SRCDIR)window_utils.c \
+					$(SRCDIR)init.c \
+					$(SRCDIR)input.c \
+					$(SRCDIR)flood.c \
+					$(SRCDIR)map.c
+SRCDIR_BONUS	=	src_bonus/
+CFILES_BONUS	=	$(SRCDIR_BONUS)so_long_bonus.c \
+					$(SRCDIR_BONUS)enemy.c
+OBJS			=	$(CFILES:.c=.o)
+OBJS_BONUS		=	$(CFILES_BONUS:.c=.o)
+INCLDIR			=	include/
+IFILES			=	$(INCLDIR)so_long.h
+LIBFTDIR		=	libftprintf/
+LIBFT			=	$(LIBFTDIR)libftprintf.a
 
-all:			$(NAME)
+all:				$(NAME)
 
 
-$(NAME):		$(OBJS) $(LIBFT)
-				$(CC) $^ -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $@
+$(NAME):			$(OBJS) $(LIBFT)
+					$(CC) $^ -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $@
 
-%.o:			%.c
-				$(CC) $(CFLAGS) -I/usr/include -Imlx_linux -c $^ -o $@
+%.o:				%.c
+					$(CC) $(CFLAGS) -I/usr/include -Imlx_linux -c $^ -o $@
 
-$(LIBFT):	 	$(LIBFTDIR)/*.c
-				cd $(LIBFTDIR) && make
+$(LIBFT):			$(LIBFTDIR)/*.c
+					cd $(LIBFTDIR) && make
 
 clean:
-				$(RM) $(RMFLAGS) $(OBJS)
-				cd $(LIBFTDIR) && make clean
-fclean:			clean
-				$(RM) $(RMFLAGS) $(NAME)
-				cd $(LIBFTDIR) && make fclean
-re:				fclean all
+					$(RM) $(RMFLAGS) $(OBJS)
+					cd $(LIBFTDIR) && make clean
+fclean:				clean
+					$(RM) $(RMFLAGS) $(NAME)
+					cd $(LIBFTDIR) && make fclean
+re:					fclean all
 
 
-.PHONY:			all clean fclean re
+bonus:				$(OBJS_BONUS) $(LIBFT)
+					$(CC) $^ -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -o $(NAME)
+
+.PHONY:				all clean fclean re bonus
---
+++ src_bonus/so_long_bonus.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/15 15:09:05 by estettle          #+#    #+#             */
+/*   Updated: 2024/12/11 19:54:21 by estettle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+void	destroy_map(char ***map)
+{
+	int	i;
+
+	i = 0;
+	while (i < MAX_TILES)
+		free((*map)[i++]);
+	free(*map);
+}
+
+/**
+ * @brief This functions writes the given string to stderr.
+ * returns it.
+ *
+ * @param str The string to output to stderr.
+ * @return The string pointer that was given as argument.
+ */
+char	*ft_perror(char *str)
+{
+	write(2, str, ft_strlen(str));
+	return (str);
+}
+
+/**
+ * @brief Kills the program after having freed mlx related variables.
+ *
+ * @param core The core struct containing mlx and other things to be freed.
+ */
+int	ft_kill(t_core *core, int err_code)
+{
+	int	i;
+
+	i = 0;
+	while (i < TEXTURE_COUNT)
+		mlx_destroy_image(core->mlx_ptr, core->textures[i++].img);
+	if (err_code != 3)
+		destroy_map(&(core->map.map));
+	if (err_code != 3 && err_code != 4)
+        mlx_destroy_window(core->mlx_ptr, core->win_ptr);
+	mlx_destroy_display(core->mlx_ptr);
+	free(core->mlx_ptr);
+	exit(err_code);
+}
+
+int	main(int argc, char **argv)
+{
+	t_core	core;
+
+	core = init_mlx();
+	if (argc != 2 || core.mlx_ptr == NULL)
+		return (free(core.mlx_ptr), -1);
+	init_textures(&core);
+	parse_map(&core, argv[1]);
+	init_player(&core);
+	init_window(&core);
+	render_map(&core);
+	mlx_hook(core.win_ptr, DestroyNotify, StructureNotifyMask, &ft_kill, &core);
+	mlx_hook(core.win_ptr, KeyPress, KeyPressMask, &key_pressed, &core);
+	mlx_loop(core.mlx_ptr);
+	return (0);
+}
