e05096754a2ff333bbae15e820fb712246ee0db8
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-02-04 14:22:42 +0100

    test script has pauses for better readability

--- test.sh
+++ test.sh
@@ -4,7 +4,18 @@ echo "[!] - Recompiling..."
 
 make re
 
-echo "[!] - Beginning error checks..."
+echo "[!] - Beginning invalid syntax checks..."
+
+read -r meh
+
+valgrind --leak-check=full ./so_long
+valgrind --leak-check=full ./so_long assets/
+valgrind --leak-check=full ./so_long assets/ladfj
+valgrind --leak-check=full ./so_long assets/maps/valid_map5
+
+echo "[!] - Beginning invalid maps checks..."
+
+read -r meh
 
 valgrind --leak-check=full ./so_long assets/maps/invalid_map1.ber
 valgrind --leak-check=full ./so_long assets/maps/invalid_map2.ber
@@ -16,7 +27,9 @@ valgrind --leak-check=full ./so_long assets/maps/invalid_map7.ber
 valgrind --leak-check=full ./so_long assets/maps/invalid_map8.ber
 valgrind --leak-check=full ./so_long assets/maps/invalid_map9.ber
 
-echo "[!] - Beginning valid maps checks..."
+read -r meh
+
+sleep 2
 
 valgrind --leak-check=full ./so_long assets/maps/valid_map1.ber
 valgrind --leak-check=full ./so_long assets/maps/valid_map2.ber
@@ -27,6 +40,8 @@ valgrind --leak-check=full ./so_long assets/maps/valid_map5.ber
 echo "[!] - Compiling bonus..."
 
 make bonus
+
+read -r meh
 
 echo "[!] - Checking bonus error checks..."
 
@@ -39,6 +54,8 @@ valgrind --leak-check=full ./so_long_bonus assets/maps/invalid_map6.ber
 valgrind --leak-check=full ./so_long_bonus assets/maps/invalid_map7.ber
 valgrind --leak-check=full ./so_long_bonus assets/maps/invalid_map8.ber
 valgrind --leak-check=full ./so_long_bonus assets/maps/invalid_map9.ber
+
+read -r meh
 
 echo "[!] - Beginning valid maps checks (bonus)..."
 
